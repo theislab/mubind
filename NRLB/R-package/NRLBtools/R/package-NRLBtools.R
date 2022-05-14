9366244f2f2b7d4e7c0450fcd359d1661b04aee4
Ignacio Ibarra <ibarra.ignacio@gmail.com>
2022-05-14 17:49:47 +0200

    removing NRLB

--- NRLB/R-package/NRLBtools/R/package-NRLBtools.R
+++
@@ -1,9 +0,0 @@
-#' NRLBtools: utitilies for working with NRLB models
-#' 
-#' The NRLB package provides tools for working with protein-DNA recognition models created by the No Read Left Behind (NRLB) algorithm developed by the Bussemaker Lab in the Department of Biological Sciences at Columbia University.
-#'
-#'
-#' @docType package
-#' @author Chaitanya Rastogi and Harmen J. Bussemaker
-#' @name NRLBtools
-NULL
--- NRLB/R-package/NRLBtools/R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onLoad <- function(libname, pkgname){
-  packageStartupMessage("Welcome to the NRLBtools package")
-}
