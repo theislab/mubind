d1ea355d00108ff2156c9ca8ff3ee1b6fe3ad7d2
Johanna Schneeberger <johanna.schneeberger@tum.de>
2022-05-20 16:07:41 +0200

    adding plotting function for the activities and notebook 12a

--- multibind/pl/plotting.py
+++ multibind/pl/plotting.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 import logomaker
 import seaborn as sns
+import numpy as np
+import torch
 
 
 def scatter(x, y):
@@ -25,6 +28,7 @@ def create_logo(net):
     
 def conv_mono(model):
     n_cols = len(model.conv_mono)
+    plt.figure(figsize=(min(6 * n_cols, 20), 5))
     for i, m in enumerate(model.conv_mono):
         # print(i, m)
         ax = plt.subplot(1, n_cols, i + 1)
@@ -39,6 +43,7 @@ def conv_mono(model):
 
 def conv_di(model):
     n_cols = len(model.conv_mono)
+    plt.figure(figsize=(min(6 * n_cols, 20), 5))
     for i, m in enumerate(model.conv_di):
         # print(i, m)
         ax = plt.subplot(1, n_cols, i + 1)
@@ -49,4 +54,18 @@ def conv_di(model):
         weights = pd.DataFrame(weights)
         weights.index = 'AA', 'AC', 'AG', 'AT', 'CA', 'CC', 'CG', 'CT', 'GA', 'GC', 'GG', 'GT', 'TA', 'TC', 'TG', 'TT'
         sns.heatmap(weights, cmap='coolwarm', center=0, ax=ax)
+    plt.show()
+
+def plot_activities(model):
+    # shape of activities: [n_libraries, len(kernels), n_rounds+1]
+    activities = np.exp(torch.stack(list(model.log_activities), dim=1).cpu().detach().numpy())
+    n_cols = activities.shape[0]
+    plt.figure(figsize=(min(6*n_cols, 20), 5))
+    for i in range(n_cols):
+        ax = plt.subplot(1, n_cols, i + 1)
+        rel_activity = activities[i, :, :] / np.sum(activities[i, :, :])
+        sns.heatmap(rel_activity.T, cmap='Reds', ax=ax)
+        plt.title('rel contrib. to lib.' + str(i))
+        plt.ylabel('selection round')
+        plt.xlabel('binding mode rel activity')
     plt.show()
