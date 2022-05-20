95f5a87eb43c9c7191881bb47e6866479b9e6e83
Johanna Schneeberger <johanna.schneeberger@tum.de>
2022-05-20 16:19:28 +0200

    adding the faster encoding method onehot_mononuc_multi

--- multibind/tl/__init__.py
+++ multibind/tl/__init__.py
@@ -13,5 +13,13 @@ from multibind.tl.prediction import (
     train_shift,
 )
 
-from multibind.tl.encoding import onehot_mononuc, onehot_covar, onehot_dinuc, onehot_mononuc_with_gaps, onehot_dinuc_with_gaps
+from multibind.tl.encoding import (
+    onehot_mononuc,
+    onehot_covar,
+    onehot_dinuc,
+    onehot_mononuc_with_gaps,
+    onehot_dinuc_with_gaps,
+    onehot_mononuc_multi
+)
+
 from multibind.tl.loss import MultiDatasetLoss, PoissonLoss
