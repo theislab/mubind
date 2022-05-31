599b661891192b32b18b0260af54904a45520dc8
Ignacio Ibarra <ibarra.ignacio@gmail.com>
2022-05-31 13:04:03 +0200

    adding 16 CTCF notebook with custom kernels

---
+++ multibind/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,21 @@
+from importlib.metadata import version
+
+from . import pl, pp, tl
+
+__all__ = ["pl", "pp", "tl", "datasets"]
+
+__version__ = version("multibind")
+
+
+import multibind
+import multibind.datasets
+import multibind.models
+import multibind.pl
+import multibind.tl
+
+try:
+    import bindome
+
+    multibind.bindome = bindome
+except ImportError:
+    print("bindome has not been installed. Please check at https://github.com/theislab/bindome")
---
+++ multibind/tl/test.py
@@ -0,0 +1,11 @@
+
+
+import numpy as np
+
+
+def supertest(m):
+    count = 0
+    for i in range(m.shape[0]):
+        for j in range(m.shape[0]):
+            count += m[i, j]
+    return np.mean(count)
