2d6647eeb4680a33c11f8ef08a9fbc3e16e92866
ege-erdogan <ege@erdogan.dev>
2022-12-29 10:12:11 +0100

    model concat refactoring

--- mubind/tl/__init__.py
+++ mubind/tl/__init__.py
@@ -27,5 +27,5 @@ from mubind.tl.prediction import (  # SelexDataset,; ChipSeqDataset,; create_dat
     scores,
     predict
 )
-from mubind.tl.aggregation import load_model, combine_models
+from mubind.tl.aggregation import concatanate, get_model_paths, get_binding_modes
 from mubind.tl.probound import load_probound
--- mubind/tl/aggregation.py
+++ mubind/tl/aggregation.py
@@ -1,51 +1,85 @@
-import numpy as np
-import pandas as pd
 import itertools
-import sklearn.metrics
-import scipy
 import torch
-import torch.optim as topti
-import torch.utils.data as tdata
 import pickle
-import os
 from pathlib import Path
 
 import mubind as mb
 
-def load_model(path):
-    with open(path, 'rb') as f:
-        model = pickle.load(f)
+
+def get_model_paths(output_path, extension='.pkl'):
+    """
+    Given a path to a directory containing model pickle files, return a list of paths to those files.
+    output_path (str): path to a directory containing model pickle files
+    extension (str): of model files (.h5 or .pkl)
+    """
+    paths = []
+    path = Path(output_path)
+    # matches all files with `extension` under `output_path`
+    for p in path.rglob("*"):
+        if extension in p.name:
+            paths.append(str(p))
+    return paths
+
+
+def get_binding_modes(items):
+    """
+    Given a list of model objects or paths to model pickle files (or mixed), return a list of binding modes.
+    items (list): list of model objects or paths to model pickle files
+    """
+    binding_modes, activations, etas = [], [], []
+    for item in items:
+        if type(item) == str:
+            with open(item, 'rb') as f:
+                model = pickle.load(f)
+        else:
+            model = item
+        binding_modes.append(model.binding_modes)
+        activations.append(model.activities.log_activities)
+        etas.append(model.selex_module._parameters['log_etas'])
+        etas 
+    return binding_modes, activations, etas
+
+
+def concatanate(binding_modes, activations, etas):
+    """
+    Combine given binding modes, activations, and etas into a single model.
+    """
+    model = mb.models.Multibind(n_rounds=1, datatype='selex')
+    del model.binding_modes.conv_mono[0:]
+    del model.binding_modes.conv_di[0:] # to remove Nones from module lists
+    for bm, a, e in zip(binding_modes, activations, etas):
+        model.binding_modes.conv_mono.extend(bm.conv_mono[1:])
+        model.binding_modes.conv_di.extend(bm.conv_di[1:])
+        model.activities.log_activities += a
+        model.selex_module._parameters['log_etas'] = model.selex_module._parameters['log_etas'].add(e)
     return model
 
 
-# currently assumes we have trained models using a single set of hyperparams 
-    # and not the grid approach (i.e. single model per setup)
-# pickle file name does not matter as long as it is somewhere under output_dir
-def combine_models(output_dir, extension='.pkl'):
+def combine_models(output_path):
     """
-    output_dir (str): some parent dir of the output directory
-    extension (str): of model files (.h5 or .pkl)
+    Given a path to a directory containing model pickle files, return a single model combining all of them.
+    output_path (str): path to a directory containing model pickle files
     """
-    models = []
-    tfs = os.listdir(output_dir)
-    path = Path(output_dir)
-    # matches all files with `extension` under `output_dir`
-    for p in path.rglob("*"):
-        if extension in p.name:
-            with open(p, 'rb') as f:
-                models.append(pickle.load(f))
-                print(f'Loaded model {p.name}')
-
-    combined_model = mb.models.Multibind(n_rounds=1, datatype='selex')
-    del combined_model.binding_modes.conv_mono[0:]
-    del combined_model.binding_modes.conv_di[0:] # to remove Nones from module lists 
-    for model in models:
-        combined_model.binding_modes.conv_mono.extend(model.binding_modes.conv_mono[1:])
-        combined_model.binding_modes.conv_di.extend(model.binding_modes.conv_di[1:])
-        combined_model.activities.log_activities += model.activities.log_activities
-        combined_model.selex_module._parameters['log_etas'] = combined_model.selex_module._parameters['log_etas'].add(model.selex_module._parameters['log_etas'])
-
-    return combined_model
+    paths = get_model_paths(output_path)
+    binding_modes, activations, etas = get_binding_modes(paths)
+    model = concatanate(binding_modes, activations, etas)
+    return model
 
         
+def weight_distances(mono, min_k=5):
+    d = []
+    for a, b in itertools.combinations(mono, r=2): # r-length combinations of elements in the iterable
+        a, b = a.weight, b.weight
+        a_width, b_width = a.shape[-1], b.shape[-1]
+        min_w = min(a_width, b_width)
 
+        for k in range(5, min_w): 
+            for i in range(0, a_width - k + 1): 
+                ai = a[:, :, :, i : i + k]
+                for j in range(0, b_width - k + 1):
+                    bi = b[:, :, :, j : j + k]
+                    bi_rev = torch.flip(bi, [3])[:, :, [3, 2, 1, 0], :]
+                    d.append(((ai-bi)**2).sum() / ai.shape[-1])
+                    d.append(((ai-bi_rev)**2).sum() / ai.shape[-1])
+
+    return min(d)
