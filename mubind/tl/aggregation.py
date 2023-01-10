b7fabc7bb0a6b692692df9a331ec4f59cfd810c9
Ignacio Ibarra <ibarra.ignacio@gmail.com>
2023-01-10 16:35:37 +0100

    fixes for min_distance function and calling

--- mubind/tl/aggregation.py
+++ mubind/tl/aggregation.py
@@ -165,44 +165,45 @@ def submatrix(m, start, length, flip, filter_neg_weights=True):
 
 
 # @jit
-def distances_dataframe(a, b, min_w_sum=3):
+def distances_dataframe(a, b, min_w_sum=0):
     d = []
     min_w = min(a.shape[-1], b.shape[-1])
+    # k = min_w
     # lowest_d = -1, -1
-    # for k in np.arange(5, min_w):
-    # print(k)
-    k = min_w
-    for i in np.arange(0, a.shape[-1] - k + 1):
-        ai = submatrix(a, i, k, 0)
-        ai_sum = ai.sum()
-        if ai_sum < min_w_sum:
-            continue
-
-        if ai_sum == 0:
-            continue
-        for j in np.arange(0, b.shape[-1] - k + 1):
-            bi = submatrix(b, j, k, 0)
-            bi_sum = bi.sum()
-            if bi_sum < min_w_sum:
+    for k in np.arange(5, min_w):
+        # print(k)
+        for i in np.arange(0, a.shape[-1] - k + 1):
+            ai = submatrix(a, i, k, 0)
+            ai_sum = ai.sum()
+            if ai_sum < min_w_sum:
                 continue
 
-            # print(type(ai), type(bi), ai.shape, bi.shape)
-            d1 = ((bi - ai) ** 2).sum() / bi.shape[-1]
-            d.append([i, j, k, ai.shape[-1], bi.shape[-1],
-                      ai.sum(), bi.sum(), 0, d1])
-            # if lowest_d[-1] == -1 or d[-1] < lowest_d[-1] or d[-2] < lowest_d[-1]:
-            #     lowest_d = i, 0, d[-1]
+            if ai_sum == 0:
+                continue
+            for j in np.arange(0, b.shape[-1] - k + 1):
+                # print(i, j)
+                bi = submatrix(b, j, k, 0)
+                bi_sum = bi.sum()
+                if bi_sum < min_w_sum:
+                    continue
 
-            bi_rev = submatrix(b, j, k, 1)
-            # flipped version
-            d2 = ((bi_rev - ai) ** 2).sum() / bi.shape[-1]
-            d.append([i, j, k, ai.shape[-1], bi.shape[-1],
-                      ai.sum(), bi.sum(), 1, d2])
-            # if lowest_d[-1] == -1 or d[-1] < lowest_d[-1] or d[-2] < lowest_d[-1]:
-            #     lowest_d = i, 1, d[-1]
+                # print(type(ai), type(bi), ai.shape, bi.shape)
+                d1 = ((bi - ai) ** 2).sum() / bi.shape[-1]
+                d.append([i, j, k, ai.shape[-1], bi.shape[-1],
+                          ai.sum(), bi.sum(), 0, d1])
+                # if lowest_d[-1] == -1 or d[-1] < lowest_d[-1] or d[-2] < lowest_d[-1]:
+                #     lowest_d = i, 0, d[-1]
 
-    res = pd.DataFrame(d, columns=['a_start', 'b_start', 'k', 'a_shape', 'b_shape',
-                                   'a_sum', 'b_sum', 'b_flip', 'distance']).sort_values('distance')
+                bi_rev = submatrix(b, j, k, 1)
+                # flipped version
+                d2 = ((bi_rev - ai) ** 2).sum() / bi.shape[-1]
+                d.append([i, j, k, ai.shape[-1], bi.shape[-1],
+                          ai.sum(), bi.sum(), 1, d2])
+                # if lowest_d[-1] == -1 or d[-1] < lowest_d[-1] or d[-2] < lowest_d[-1]:
+                #     lowest_d = i, 1, d[-1]
+
+        res = pd.DataFrame(d, columns=['a_start', 'b_start', 'k', 'a_shape', 'b_shape',
+                                       'a_sum', 'b_sum', 'b_flip', 'distance']).sort_values('distance')
     return res
 
 def calculate_distances(mono_list, full=False, best=False):
@@ -321,8 +322,8 @@ def reduce_filters(binding_modes, plot=False, thr_group=0.01):
     return monos
 
 
-def min_distance(w1, w2):
-    df = distances_dataframe(w1, w2)
+def min_distance(w1, w2, **kwargs):
+    df = distances_dataframe(w1, w2, **kwargs)
     if df.shape[0] == 0:
         return np.nan
     return min(df['distance'])
