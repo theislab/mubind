09c7dedf533a5fd56e7760bb52cbfedb1d1e7d8a
Ignacio Ibarra <ibarra.ignacio@gmail.com>
2024-04-09 12:44:51 +0200

    fix import (for read the docs)

--- mubind/__init__.py
+++ mubind/__init__.py
@@ -6,17 +6,15 @@ __all__ = ["pl", "pp", "tl", "get", "set", "datasets"]
 
 __version__ = version("mubind")
 
-
-import mubind
-import mubind.datasets
-import mubind.models
-import mubind.pl
-import mubind.tl
-import mubind.get
+from . import datasets
+from . import models
+from . import pl
+from . import tl
+from . import get
 
 try:
     import bindome
-
     mubind.bindome = bindome
+
 except ImportError:
     print("bindome has not been installed. Please check at https://github.com/theislab/bindome")
